e145944eb44ae1cc0a1dbc7aa25590746f4cf018
Justsneak7 <71436825+Justsneak7@users.noreply.github.com>
2020-12-03 11:29:51 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -55,7 +55,7 @@ class Game{
                      x = 500-allPlayers[plr].distance;
                      y=500;
                      
-                     players[index -1].x = x;
+                     oooplayers[index -1].x = x;
                      players[index - 1].y = y;
                        
                      if(index === player.index){
